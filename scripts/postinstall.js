ee3423733df166e829dc3fbe675a449a41994a0c
clement <maboiteaspam@wanadoo.fr>
2014-05-13 22:51:59 +0800

    better install process

---
+++ scripts/postinstall.js
@@ -0,0 +1,13 @@
+
+var exec = require('child_process').exec,
+  child;
+
+child = exec('youtube-dl --version',
+  function (error, stdout, stderr) {
+    if (error !== null) {
+      console.error('youtube-dl not found in your system');
+      console.info('stdout: ' + stdout);
+      console.info('stderr: ' + stderr);
+      console.info('exec error: ' + error);
+    }
+  });
